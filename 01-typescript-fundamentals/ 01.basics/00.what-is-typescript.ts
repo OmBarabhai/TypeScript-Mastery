24ddc160fe81f8d055b2cce7e4620992bb6f8e1c
OmBarabhai <oraghuwanshi37@gmail.com>
2025-02-13 20:45:42 +0530

    Delete 01-typescript-fundamentals/ 01.basics directory

--- 01-typescript-fundamentals/ 01.basics/00.what-is-typescript.ts
+++
@@ -1,5 +0,0 @@
-// What is TypeScript?
-// TypeScript is a superset of JavaScript that adds static typing.
-
-let message: string = "Hello, TypeScript!";
-console.log(message);
--- 01-typescript-fundamentals/ 01.basics/02-any-type.ts
+++
@@ -1,5 +0,0 @@
-// Any Type - Can store any value
-let color: any = "crimson";
-color = 20;  // Valid
-color = true;  // Valid
-console.log(color);
