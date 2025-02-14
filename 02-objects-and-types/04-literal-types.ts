9a61d8184ccc3028f9c5ab650cf6d947ba92af0b
OmBarabhai <oraghuwanshi37@gmail.com>
2025-02-14 08:30:57 +0000

    literals

---
+++ 02-objects-and-types/04-literal-types.ts
@@ -0,0 +1,12 @@
+let color: 'red' | "crimson" | "fusha"
+color = "red";
+// color = "yellow";
+console.log(color);
+
+let isTrue: true| false;
+isTrue = true;
+// isTrue='somethingelse'
+
+let password: "secretpass" = "secretpass";
+// password = "something else";
+console.log(password);
